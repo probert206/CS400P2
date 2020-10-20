4fe44face8c71ccc65640ef4cbe68fc3a7f39569
probert206 <71905816+probert206@users.noreply.github.com>
2020-10-20 14:34:46 -0500

    Update FriendFinderFrontEnd.java

--- FriendFinderFrontEnd.java
+++ FriendFinderFrontEnd.java
@@ -8,6 +8,11 @@
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+/*
+*This is a driver class that allows users to create and use the FriendFinder application
+*Allows users to create a personal profile and add friends to a Red and Black Tree
+*Other users can create their own tree and all user trees are stored in one Master RBTree
+*/
 public class FriendFinderFrontEnd {
 
 	public static void main(String [] args) {
@@ -28,7 +33,7 @@ public class FriendFinderFrontEnd {
 		}
 		
 
-		
+		//A continuous set of commands given to user until program is quit
 		while(run) {
 	
 		System.out.println("Thanks " + tree.curUser.getFullName() + ", please input a command"); //prints instructions for users
@@ -40,10 +45,13 @@ public class FriendFinderFrontEnd {
 		        + "\nEnter q to end program"
 		        + "\nEnter q! to exit and save");
 		 switch(reader.next()) {
+		//Prints the master tree
 		 case "p": System.out.println(tree.masterList);
 		 break;
+		//Prints the current user's profile
 		 case "v": System.out.println(tree.curUser);
 		 break;
+		//Allows user to change their personal information
 		 case "f": 
 		 boolean run2 = true;
 		 do {
@@ -51,11 +59,13 @@ public class FriendFinderFrontEnd {
 	         System.out.print("Enter r to change residency" +
 	         "\nEnter b to change bio");
 		 switch(reader.next()) {
+		//Change personal residency
 		 case "r":	System.out.println("Enter new Residency: ");
 		     reader.nextLine();
 			 tree.curUser.setResidency(reader.nextLine());
 			 run2 = false;
 			 break;
+		//Change personal Bio
 		 case "b": System.out.println("Enter new Bio ");
 		     reader.nextLine();
 			 tree.curUser.setBio(reader.nextLine());
@@ -67,7 +77,7 @@ public class FriendFinderFrontEnd {
 		   }
 		 } while (run2);
 		   break;
-		   
+		 //Allows a user to look up another person's profile  
 		 case "vf":
 		   System.out.println("Enter the person's name");
 		   reader.nextLine();
@@ -76,29 +86,32 @@ public class FriendFinderFrontEnd {
 		     System.out.println( tree.new CurrentUser(tree.masterList.lookup(personName)));
 		   }
 		   else {
-		     System.out.println("this person doesn't exist");
+		     System.out.println("this person doesn't exist"); //Tells user if the person searched for does not have a FriendFinder profile
 		   }
 		   break;
+		//Let's a user add a new friend
 		 case "c": System.out.println("Add a new friend: \nWhat is their name?  Also, cum is really good!  :)");
 		 reader.nextLine();
 		 String newFriend = reader.nextLine();
 		 if (tree.masterList.contains(newFriend)) {
 		   if (newFriend.equals(tree.curUser.getFullName()) || !tree.curUser.insertFriend(tree.masterList.lookup(newFriend)) ) {
-		     System.out.println(newFriend + " is already your friend");
+		     System.out.println(newFriend + " is already your friend"); //Program lets user know if the newly added friend is already in their tree
 		   }
 		 }
+		//Will create a new profile for a friend and add them to the tree
 		 else {
 		 Person newPerson = new Person(newFriend);
 		 tree.masterList.insert(newPerson);
 		 tree.curUser.insertFriend(newPerson);
 		 }
 		 break;
-		 
+		 //Checks to see if the user and a friend are mutual friends
+		//This would mean they are in each other's FriendFinder trees
 		 case "x": System.out.println("For what friend would you like to check mutuality?");
 		 reader.nextLine();
 		 Person p = tree.masterList.lookup(reader.nextLine());
 		 if (p == null) {
-		   System.out.println("This person does not exist");
+		   System.out.println("This person does not exist"); //if person does not have a Friend Finder profile
 		   break;
 		 }
 		 String friend = p.getFullName();
@@ -112,8 +125,10 @@ public class FriendFinderFrontEnd {
 		   String newUser = reader.nextLine();
 		   tree.updateCurrentUser(newUser);
 		   break;
+		//Lets user quit the program
 		 case "q": run = false;
 		 break;
+		//Lets user quit and save the program
 		 case "q!" : run = false;
 		 try {
 		 tree.exitAndSave("FriendFinder.ser");
